39882299ee4e47de9c52e8ce65ed59ba99ba4754
qjumplife <twohotdog@outlook.com>
2025-11-08 21:46:48 +0800

    Add reliable WebSocket trackers

--- src/config/trackerUrls.ts
+++ src/config/trackerUrls.ts
@@ -1,5 +1,21 @@
-// 使用 undefined 让 Trystero 使用默认的 nostr 策略（不需要 tracker）
-let trackerUrls: string[] | undefined = undefined
+// WebTorrent tracker 列表（必须是 wss:// 协议）
+let trackerUrls: string[] | undefined = [
+  // 国内可访问的 tracker
+  'wss://tracker.sloppyta.co:443/announce',
+  'wss://tracker.novage.com.ua:443/announce',
+  'wss://tracker.ghostchu-services.top:443/announce',
+
+  // 备用 tracker
+  'wss://tracker.openwebtorrent.com:443/announce',
+  'wss://tracker.webtorrent.dev:443/announce',
+  'wss://tracker.files.fm:7073/announce',
+  'wss://tracker.btorrent.xyz:443/announce',
+  'wss://tracker.openwebtorrent.com',
+  'wss://tracker.btorrent.xyz',
+  'wss://qot.abir.top:443/announce',
+  'wss://spacetradersapi-chatbox.herokuapp.com:443/announce'
+  
+]
 
 // If a tracker URL has been provided via the VITE_TRACKER_URL environment
 // variable, prioritize using it. This is mainly relevant for local development
