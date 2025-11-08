64287a373fac2af8d57b96d2444c84d153ca83e7
qjumplife <twohotdog@outlook.com>
2025-11-08 22:11:12 +0800

    Use Trystero default trackers

--- src/config/trackerUrls.ts
+++ src/config/trackerUrls.ts
@@ -1,5 +1,5 @@
-// 使用 undefined 让 Trystero 使用 nostr 策略（不需要 tracker，适合国内网络）
-// nostr 通过中继服务器建立 P2P 连接，更稳定且不被墙
+// 使用 undefined 让 Trystero 使用默认 tracker 列表
+// 默认列表: https://github.com/dmotz/trystero/blob/main/src/torrent.js#L6
 let trackerUrls: string[] | undefined = undefined
 
 // If a tracker URL has been provided via the VITE_TRACKER_URL environment
@@ -7,13 +7,7 @@ let trackerUrls: string[] | undefined = undefined
 // when using the `npm run dev` script. If you are hosting your own Chitchatter
 // instance, consider populating the trackerUrls above instead.
 if (import.meta.env.VITE_TRACKER_URL) {
-  trackerUrls.unshift(import.meta.env.VITE_TRACKER_URL)
-}
-
-// If no tracker URL overrides have been provided, set trackerUrls to undefined
-// to allow Trystero to use the default list (linked above).
-if (!trackerUrls.length) {
-  trackerUrls = undefined
+  trackerUrls = [import.meta.env.VITE_TRACKER_URL]
 }
 
 export { trackerUrls }
